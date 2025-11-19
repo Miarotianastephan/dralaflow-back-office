b13cb60eba9cdb2d1343d20bee1f6994d40982ca
Miarotiana Stephan <miarotianastephan01@gmail.com>
2025-11-19 14:02:36 +0300

    part complete of category

--- app/Filament/Resources/ExpenseCategories/ExpenseCategoryResource.php
+++ app/Filament/Resources/ExpenseCategories/ExpenseCategoryResource.php
@@ -13,6 +13,7 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class ExpenseCategoryResource extends Resource
 {
@@ -21,6 +22,8 @@ class ExpenseCategoryResource extends Resource
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
     protected static ?string $recordTitleAttribute = 'Expense category';
+
+    protected static string|UnitEnum|null $navigationGroup = 'Administration';
 
     public static function form(Schema $schema): Schema
     {
